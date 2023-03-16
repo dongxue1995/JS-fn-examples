331519c54bf5b098e2cc3ad7e1a7052eacf22557
Wu Dongxue <91260941+dongxue1995@users.noreply.github.com>
2023-03-16 09:21:05 +0100

    refine

---
+++ Promise-class.js
@@ -0,0 +1,26 @@
+class Commnitment {
+  static pending = "pending";
+  static fullfilled = "fulllfilled";
+  static rejected = "rejected";
+  constructor(fn) {
+    this.status = Commnitment.pending; //set as default
+    this.result = null;
+    fn(this.res, this.rej);
+  }
+  res(result) {
+    if (this.status === Commnitment.pending) {
+      this.status = Commnitment.fullfilled;
+      this.result = result;
+    }
+  }
+  rej(result) {
+    if (this.status === Commnitment.pending) {
+      this.status = Commnitment.rejected;
+      this.result = result;
+    }
+  }
+}
+
+let newCommnitment = new Commnitment((res, rej) => {
+  res("success");
+});
